ea80f6fd4abb0e9f966ecb9993d28a3baf37863e
Dmitry Kurinskiy <name.alari@gmail.com>
2014-11-04 14:25:02 +0300

    moveToCart / saveForLater

--- src/main/scala/carryx/amazon/api/operation/CartModify.scala
+++ src/main/scala/carryx/amazon/api/operation/CartModify.scala
@@ -17,15 +17,46 @@ object CartModify {
    * @param items pairs of (cart item id -> quantity)
    * @return
    */
-  def modify(cartId: String, hmac: String, items: (String, Int)*) = AmazonRequest(
-     "CartModify",
+  def modify(cartId: String, hmac: String, items: (String, Int)*) = modifyRequest(cartId, hmac, items, None)
 
-      items.view.zipWithIndex.foldLeft(Map(
-        "CartId" -> cartId,
-        "HMAC" -> hmac
-      )) {
-    case (m, ((cartItemId, q), i)) =>
-      m + (s"Item.$i.CartItemId" -> cartItemId) + (s"Item.$i.Quantity" -> q.toString)
-  }
+  /**
+   * Move SavedForLater items to actual cart
+   *
+   * @param cartId cart id
+   * @param hmac cart hmac
+   * @param items pairs of (cart item id -> quantity)
+   * @return
+   */
+  def moveToCart(cartId: String, hmac: String, items: (String, Int)*) = modifyRequest(cartId, hmac, items, Some("MoveToCart"))
+
+  /**
+   * Move actual cart item to SavedForLater list
+   *
+   * @param cartId cart id
+   * @param hmac cart hmac
+   * @param items pairs of (cart item id -> quantity)
+   * @return
+   */
+  def saveForLater(cartId: String, hmac: String, items: (String, Int)*) = modifyRequest(cartId, hmac, items, Some("SaveForLater"))
+
+  /**
+   * Helper method for any CartModify request
+   *
+   * @param cartId cart id
+   * @param hmac cart hmac
+   * @param items pairs of (cart item id -> quantity)
+   * @param action an action to add to all items
+   * @return
+   */
+  def modifyRequest(cartId: String, hmac: String, items: Seq[(String, Int)], action: Option[String]) = AmazonRequest(
+    "CartModify",
+
+    items.view.zipWithIndex.foldLeft(Map(
+      "CartId" -> cartId,
+      "HMAC" -> hmac
+    )) {
+      case (m, ((cartItemId, q), i)) =>
+        action.fold(m)(a => m + (s"Item.$i.Action" -> a)) + (s"Item.$i.CartItemId" -> cartItemId) + (s"Item.$i.Quantity" -> q.toString)
+    }
   )
 }
